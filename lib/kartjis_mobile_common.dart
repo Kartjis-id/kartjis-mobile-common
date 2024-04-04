2a1f0bc76a3887a402d1d15f832ebaccfe295ec2
toku404 <phantom26isn@gmail.com>
2024-04-04 23:52:24 +0800

    fix export

--- lib/kartjis_mobile_common.dart
+++ lib/kartjis_mobile_common.dart
@@ -1,7 +1,3 @@
 library kartjis_mobile_common;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/ui/components/_widgets.dart';
