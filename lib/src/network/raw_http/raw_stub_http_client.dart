64862b3a640aa7ac13b1f064df14134acc5a5af5
toku404 <phantom26isn@gmail.com>
2024-04-05 05:10:46 +0800

    fix raw http

--- lib/src/network/raw_http/raw_stub_http_client.dart
+++ lib/src/network/raw_http/raw_stub_http_client.dart
@@ -1,4 +1,4 @@
-
 import 'package:kartjis_mobile_common/src/network/raw_http/raw_http_client.dart';
 
-RawHttpClient createClient() => throw UnsupportedError('Cannot create a client without dart:html or dart:io.');
+RawHttpClient createClient() => throw UnsupportedError(
+    'Cannot create a client without dart:html or dart:io.');
